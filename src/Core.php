ce16908b64a658ccc8d74f41edde6216570f8618
Kirillovich <kir3316@yandex.ru>
2023-04-06 03:35:23 +0500

    game Even

---
+++ src/Core.php
@@ -0,0 +1,35 @@
+<?php
+
+// phpcs:disable PSR1.Files.SideEffects
+namespace BrainGames\Core;
+
+use function cli\line;
+use function cli\prompt;
+
+function core(string $rules, array $data)
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?', false, ' ');
+    line("Hello, {$name}!");
+
+    $lengthEnd = count($data) - 1;
+    $rounds = 3;
+
+    while ($rounds > 0) {
+        $position = rand(0, $lengthEnd);
+        [$question, $verity] = $data[$position];
+        line("Question: {$question}");
+        $answer = prompt('Your answer:', false, ' ');
+        if ($answer === $verity) {
+            line('Correct!');
+            if ($rounds === 1) {
+                line("Congratulations, {$name}!");
+            }
+            $rounds--;
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$verity}'.");
+            line("Let's try again, {$name}!");
+            break;
+        }
+    }
+}
---
+++ src/games/Even.php
@@ -0,0 +1,33 @@
+<?php
+
+// phpcs:disable PSR1.Files.SideEffects
+namespace BrainGames\Games\Even;
+
+require_once './src/Core.php';
+
+use function BrainGames\Core\core;
+
+const RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+function generateData($length = 10)
+{
+    $data = [];
+    $count = 0;
+
+    while ($count < $length) {
+        $num = rand(1, 15);
+        if ($num % 2 === 0) {
+            $data[] = [$num, 'yes'];
+        } else {
+            $data[] = [$num, 'no'];
+        }
+        $count++;
+    }
+
+    return $data;
+}
+
+function play()
+{
+    core(RULES, generateData());
+}
